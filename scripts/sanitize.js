581c2c8f80bb9993bd37f2268b9b004911803694
Gerad Suyderhoud <gerad@fortnightlabs.com>
2012-10-29 09:25:46 -0700

    don't sanitize visnu/gerad gmail addresses

--- scripts/sanitize.js
+++ scripts/sanitize.js
@@ -1,3 +1,8 @@
-db.people.find({ email: { $nin: [ /@fortnightlabs\.com$/, /\.nodeknockout.com$/] }}).forEach(function(doc) {
-  db.people.update({ _id: doc._id }, { $set: { email: doc.email + '.nodeknockout.com' }});
+db.people.find({ email: { $nin: [
+      /^visnupx?@gmail.com$/,
+      'gerads@gmail.com',
+      /@fortnightlabs\.com$/,
+      /\.nodeknockout.com$/] }}).forEach(function(doc) {
+  db.people.update({ _id: doc._id },
+    { $set: { email: doc.email + '.nodeknockout.com' }});
 });
